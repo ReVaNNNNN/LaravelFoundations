d532c42959d81a467933e34c3679ce2ff547c496
ksobecki <revannn5480@gmail.com>
2019-09-11 07:28:36 +0200

    ProjectFactory implementation.

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -3,10 +3,14 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Project;
+use App\Task;
 use Faker\Generator as Faker;
 
 $factory->define(Project::class, function (Faker $faker) {
     return [
-
+        'id' => $faker->numberBetween(1, 1000),
+        'title' => $faker->realText(),
+        'description' => $faker->realText(),
+        'task' => new Task()
     ];
 });
